ae63dd7a066db92c2077bbe98a8574f9514379fb
Shruti3004 <mail2shruti.ag@gmail.com>
2020-08-02 02:46:05 +0530

    added basic structure with react

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import LandingPage from './components/LandingPage';
+import Default from './components/Default';
+import Contact from './components/Contact';
+import About from './components/About';
+import Navbar from './components/Navbar';
+
+function App() {
+  return(
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={LandingPage} />        
+        <Route path="/about" component={About} />        
+        <Route path="/contact" component={Contact} />
+        <Route component={Default} />
+      </Switch>      
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Default.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Default() {
+    return (
+        <div className="d-flex justify-content-center">
+            <h1>Page Not Found</h1>   
+        </div>
+    )
+}
+
+export default Default
