6779543c19edefb0f5ce385b1ca07c3139308d3f
Shankhanil Borthakur <shankhanilborthakur@gmail.com>
2020-08-13 21:45:45 +0530

    Added formspree for sending response to specific id

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import React from "react";
 
-function Contact() {
+export default class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: "",
+    };
+  }
+
+  render() {
+    const { status } = this.state;
     return (
-        <>
-            <h1 className="text-center">Contact</h1>
-        </>
-    )
-}
+      <div class="container">
+        <form
+          onSubmit={this.submitForm}
+          action="https://formspree.io/xzbjwkae"
+          method="POST"
+        >
+          <div class="form-group">
+            <label>Email</label>
+            <input
+              class="form-control"
+              placeholder="Enter email"
+              type="email"
+              name="email"
+            />
+          </div>
 
-export default Contact
+          <div class="form-group">
+            <label>Message</label>
+            <textarea
+              name="message"
+              rows="3"
+              cols="30"
+              class="form-control"
+              placeholder="Enter message"
+            ></textarea>
+          </div>
+
+          {status === "SUCCESS" ? (
+            <p>Thanks!</p>
+          ) : (
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          )}
+          {status === "ERROR" && <p>Ooops! There was an error.</p>}
+        </form>
+      </div>
+    );
+  }
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
+}
