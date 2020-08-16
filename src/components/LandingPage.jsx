4090aeba8e284e828fddee3a8498eb9b79647b0d
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2020-08-16 19:23:15 +0530

    implemented Heading, Description and Video on the Home Page
    Implemented Basic Description Text of the Home page.
    
    Also added video player on the Home page.
    (It plays a dummy video right now. Later on the source Path of the video would be changed)

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import "../../node_modules/video-react/dist/video-react.css"; // import css
+// import "~video-react/styles/scss/video-react"; // or import scss
+import { Player } from 'video-react';
+import classes from './assets/css/LandingPage.module.css'
 import styled from 'styled-components';
 
 function LandingPage() {
@@ -7,6 +11,21 @@ function LandingPage() {
             <Wrapper>
                 <h1 className="text-center">Social Schedular</h1>
             </Wrapper>
+            <div className={classes.Description}>
+                <h2>Social Media is everywhere.<br/>Why not to use it for events management?</h2>
+                <h3>Fill the from to get the updates (like deadlines, important announcments) on your Social Media.</h3>
+                <div className={classes.Player}>
+                    <Player
+                        playsInline
+                        autoPlay={true}
+                        // fluid={false}
+                        // width={'70%'}
+                        // height={'auto'}
+                        poster="/assets/poster.png"
+                        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+                    />
+                </div>
+            </div>
         </>
     )
 }
