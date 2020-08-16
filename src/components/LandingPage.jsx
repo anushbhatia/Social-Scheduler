d11f047821ab9682077b5c8104f1235829d7c3bc
Souvik <souvikde.ns@gmail.com>
2020-08-16 22:09:59 +0530

    Update LandingPage.jsx

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
-import useContri from '../lib/contributers'
 
 function LandingPage() {
-    let [data, err] = useContri()
-    console.log(data) 
     return (
         <>
             <Wrapper>
