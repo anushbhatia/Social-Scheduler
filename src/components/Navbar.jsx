095a9d57bfbfe93ed7df66ac9965bd857171e20d
chetna <chetnavats11@gmail.com>
2020-08-19 22:23:38 +0530

    removed extra navbar from the file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,34 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { FaPaperPlane } from "react-icons/fa";
 
-function Navbar() {
-    return(
-        <NavWrapper className="navbar navbar-expand-sm navbar-light px-sm-5">  
-            <div className="ml-auto navbar-nav">
-                <Link to="/">
-                    <div className="nav-item text-dark px-4 font-weight-bold">
-                        Home
-                    </div>
-                </Link>
-                <Link to="/about">
-                    <div className="nav-item text-dark px-4 font-weight-bold">
-                        About
-                    </div>
-                </Link>
-                <Link to="/contact">
-                    <div className="nav-item text-dark px-4 font-weight-bold">
-                        Contact
-                    </div>
-                </Link>
-                <Link to="/detail">
-                    <div className="nav-item text-dark px-4 font-weight-bold">
-                        Contributers
-                    </div>
-                </Link>
-            </div>            
-        </NavWrapper>
-    )
-}
+
 
 const Navbar = () => {
 
@@ -39,8 +12,11 @@ const Navbar = () => {
     { name: "Home", path: "/" },
         { name: "Contact", path: "/contact" },
         { name: "About", path: "/about" },
-    ];
+        { name: "Contributors", path: "/detail" },
 
+        
+    ];
+              
     return (
       <nav className="navbar navbar-expand-lg navbar-light shadow mb-3 py-3 bg-white">
         <div className="container">
