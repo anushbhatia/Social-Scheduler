a42252dd27f30774f125122834503a74233720c4
kashika3101 <kashikajain3101@gmail.com>
2020-08-21 23:10:01 +0530

    Added the 404 error component

--- src/components/Page_404/Error.jsx
+++ src/components/Page_404/Error.jsx
@@ -2,23 +2,29 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Error.css";
 import download from "./404.png";
-import notfoundimg from "./PageNotFound.png";
 
 function Error() {
 return (
-<div className="style">
-    <img src={download} alt="" /> <h1>Error</h1>
-    <img src={notfoundimg} alt="" id="styling" />
-    <div className="text">
-       Sorry! The page does not exist.
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <img src={download} alt="" />
+
+    <div className="wrapper">  
+    <div className="line"></div>
+     <div className="button">404 ERROR !</div>
     </div>
-<hr />
-    <p>
-    <NavLink to="/" className="button">
-          Go Back{" "}
-    </NavLink>
-    </p>
+    
+    <p className="content">WE SINCERELY APOLOGIZE</p>
+    <p>Uh oh, we can't seem to find the page you're looking for.</p>
+    <p> Try going back to the previous page for more information.</p>
+    <div className="wrapper1">  
+    <div className="line1">
+      </div><NavLink className="button1" to="/"> Go Home </NavLink></div>
+    </div>
+  </div>
 </div>
   );
 }
 export default Error;
+
